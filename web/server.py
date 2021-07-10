10e1cf61bd77b46324411fefc37a99d585469a15
OnceLearner <redaamir972@gmail.com>
2021-07-10 13:31:00 +0100

    adding all features :

--- web/server.py
+++ web/server.py
@@ -1,15 +1,22 @@
 import os
 
 from flask import Flask , flash, request, redirect, url_for,send_file, send_from_directory, safe_join, abort,jsonify
-
+from firebase_admin import credentials, firestore, initialize_app
 from flask_cors import CORS, cross_origin
-
+import time
 from bsMethods import scrap_emploi,scrap_announces_url_into_array
 
 app = Flask(__name__)
 app.config['CORS_HEADERS'] = 'Content-Type'
 CORS(app)
 
+
+
+# Initialize Firestore DB
+cred = credentials.Certificate('key.json')
+default_app = initialize_app(cred)
+db = firestore.client()
+emploi_ref = db.collection('emploi')
 
 
 @app.route('/test', methods=['GET'])
@@ -43,8 +50,69 @@ def scrap_annonces():
 
 
 
+#post new job
+@cross_origin()
+@app.route("/emploi/jobs",methods=["POST"])
+def save_one_job():
+    array_of_data = request.get_json()
+    print(array_of_data)
+    try:
+        id = "emploi" +str(int(time.time()))
+        print(id)
+        emploi_ref.document(id).set(request.json)
+        return jsonify({"success": True}), 200
+    except Exception as e:
+        return f"An Error Occured: {e}"
+
+
+
+
+#get one or all jobs
+@cross_origin()
+@app.route('/emploi/jobs', methods=['GET'])
+def read_jobs():
+    """
+        read() : Fetches documents from Firestore collection as JSON.
+        todo : Return document that matches query ID.
+        all_todos : Return all documents.
+    """
+    try:
+        # Check if ID was passed to URL query
+        job_id = request.args.get('id')
+        if job_id:
+            todo = emploi_ref.document(job_id).get()
+            return jsonify(todo.to_dict()), 200
+        else:
+            all_jobs = [doc.to_dict() for doc in emploi_ref.stream()]
+            return jsonify(all_jobs), 200
+    except Exception as e:
+        return f"An Error Occured: {e}"
+
+
+
+
+
+#delete a job by id
+@cross_origin()
+@app.route('/emploi/jobs', methods=['DELETE'])
+def delete():
+    """
+        delete() : Delete a document from Firestore collection.
+    """
+    try:
+        # Check for ID in URL query
+        job_id = request.args.get('id')
+        emploi_ref.document(job_id).delete()
+        return jsonify({"success": True}), 200
+    except Exception as e:
+        return f"An Error Occured: {e}"
+
+
+
+
+
 port = int(os.environ.get('PORT', 5000))
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True,port=port,debug=True)
+    app.run(threaded=True,port=port)
